619c0ecff3aa57029c0e9c5fe15c3bfb5ee91bbf
Nicolasfelixvieira <00001063813748sp@al.educacao.sp.gov.br>
2024-12-07 01:33:14 -0300

    Add files via upload

---
+++ Nicolas/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Vingadores',
+    'Quem é o líder dos Vingadores?',
+    'O líder dos Vingadores é o Capitão América.'
+);
+
+criaCartao(
+    'Vingadores',
+    'Qual é o nome da base dos Vingadores?',
+    'A base dos Vingadores é a Torre dos Vingadores.'
+);
+
+criaCartao(
+    'Vingadores',
+    'Quem é o deus do trovão no grupo dos Vingadores?',
+    'O deus do trovão é Thor.'
+);
+
+criaCartao(
+    'Vingadores',
+    'Qual é o nome do traje tecnológico do Homem de Ferro?',
+    'O traje tecnológico do Homem de Ferro é a Armadura Mark.'
+);
